92ec29e538499d671603dcef5a9566a589a52f51
MrRyanAlexander <lazyguymedia@gmail.com>
2015-05-04 15:11:17 -0700

    Fixed Commments

--- juggle.py
+++ juggle.py
@@ -17,27 +17,50 @@ Given 12K jugglers and 2K cicuits, we know to assume
 there will be 6 jugglers accepted into each circuit. 
 
 '''
-import re 											#import the regex package
+#import the regex package
+import re 											
 
-circuits 		= []								#list to hold all the circuits
-circuitCount 	= 0 								#track total circuits created
-jugglerCount 	= 0 								#track total jugglers created
-hazeCount    	= 0 								#track overal hazing total
+#list to hold all the circuits
+circuits = []		
 
-class Circuit:										#Class Object for circuits
+#track total circuits created			
+circuitCount = 0 								
+
+#track total jugglers created
+jugglerCount = 0 		
+
+#track overal hazing total
+hazeCount = 0 								
+
+#Class Object for circuits
+class Circuit:										
 
 	def __init__(self, data):
 		global circuitCount
 
-		self.t = data[0]							#circuit name value
-		self.h = data[1]  							#hand eye
-		self.e = data[2]							#endurance
-		self.p = data[3]							#pizzaz
-		self.matches = []							#the 4-6 matches for this circuit
-		self.hasMatch = False						#track if circuit has match
-		circuitCount+=1 							#track total circuits created
+		#circuit name value
+		self.t = data[0]	
 
-	def hazeInductee(self, j):						#haze an inductee; a juggler
+		#hand eye
+		self.h = data[1]  							
+		
+		#endurance
+		self.e = data[2]							
+		
+		#pizzaz
+		self.p = data[3]							
+		
+		#the 4-6 matches for this circuit
+		self.matches = []							
+		
+		#track if circuit has match
+		self.hasMatch = False						
+		
+		#track total circuits created
+		circuitCount+=1 							
+
+	#haze an inductee; a juggler
+	def hazeInductee(self, j):						
 		global hazeCount
 
 		hazeCount +=1
@@ -61,32 +84,59 @@ class Circuit:										#Class Object for circuits
 		else:
 			#print "Appending first juggler {0} to enter circuit {1}".format(j.t, self.t)
 			
-			self.matches.append(j)					#add the first juggler to this circuit
-			self.hasMatch = True					#flip the swith for has matches only run 
-												    #this once per circuit match
+			#add the first juggler to this circuit
+			self.matches.append(j)				
 
+			#flip the swith for has matches only run 
+			#this once per circuit match
+			self.hasMatch = True					
+
+	#insert match at index
 	def insertMatch(self, j, i):
+
+		#if there is no index
 		if i == None:
-			self.matches.append(j)					#append the new juggler to the list
+
+			#append the new juggler to the list
+			self.matches.append(j)					
 		else:	
-			self.matches.insert(i, j)				#insert the higher match, bumping the rest down 1
 
-		if len(self.matches) > 6:					#send extra jugglers to their next preference
-			tj = self.matches[-1]					#copy the juggler
-			self.matches.remove(self.matches[-1])	#remove the juggler for this circuit
-			self.passJuggler(tj)					#send the juggler to its next circuit
+			#insert the higher match, bumping the rest down 1
+			self.matches.insert(i, j)				
 
-	def passJuggler(self, j):						#pass juggler to its next preference
-		if not j.sp >= len(j.dps)-1:				#make sure not to pass jugglers out of bounds
-			j.sp +=1 								#increment the jugglers current preferred circuit
-													#for the next loop after it's passed
+		#send extra jugglers to their next preference
+		if len(self.matches) > 6:			
 
-													#pass the juggler to next circuit
+			#copy the juggler		
+			tj = self.matches[-1]			
+
+			#remove the juggler for this circuit		
+			self.matches.remove(self.matches[-1])	
+
+			#send the juggler to its next circuit
+			self.passJuggler(tj)					
+
+	#pass juggler to its next preference
+	def passJuggler(self, j):	
+
+		#make sure not to pass jugglers out of bounds					
+		if not j.sp >= len(j.dps)-1:	
+
+			#increment the jugglers current preferred circuit	
+			#for the next loop after it's passed		
+			j.sp +=1 								
+													
+			#pass the juggler to next circuit
 			circuits[j.dpsKeyForIndex(j.sp)].hazeInductee(j)
 
+	#test juggler against list for match
 	def isMatch(self, j):
-		notFound=True								#keep track if no match is found
-		for i in range(len(self.matches)):			#compare to all index
+
+		#keep track if no match is found
+		notFound=True								
+
+		#compare to all index
+		for i in range(len(self.matches)):			
 			
 			#print "Val for J {0} compared to C {1}".format(j.dpsValForIndex(j.sp), self.matches[i].dpsValForCirKey(self.t))
 			
@@ -94,101 +144,174 @@ class Circuit:										#Class Object for circuits
 				
 				#print "Picked Juggler {0} with value {1} compared to {2}".format(j.t, j.dpsValForIndex(j.sp), self.matches[i].dpsValForCirKey(self.t))
 			
-				notFound=False						#flip the switch if found
-				self.insertMatch(j, i)				#start inserting the match at index
+				#flip the switch if found
+				notFound=False			
+
+				#start inserting the match at index
+				self.insertMatch(j, i)				
+
+				#break out of loop
 				break
 
-		if notFound:								#if no match was found looping
-			self.insertMatch(j, None)				#the match will just be appended
+		#if no match was found looping
+		if notFound:							
 
-	def cmnt(self):									#add up the matched juggler names
-		t = 0 										#this is just used at the end to 
-		for j in self.matches:						#print out something pretty
-			t += j.t 								#adds to the total name count
-		return t 									#return the total
+			#the match will just be appended	
+			self.insertMatch(j, None)				
 
-class Juggler:										#Class Object for jugglers
+	#add up the matched juggler names
+	#this is just used at the end to
+	#print out something pretty
+	def cmnt(self):									
+		t = 0 										 
+		for j in self.matches:				
+
+			#adds to the total name count		
+			t += j.t 						
+
+		#return the total		
+		return t 		
+
+#Class Object for jugglers
+class Juggler:							
+
 	def __init__(self, data):
 		global jugglerCount
 
-		self.t   = data[0]							#juggler name value
-		self.dps = self.dotProducts(data)			#list of dot products
-		self.sp  = 0 								#track the preference order
-		jugglerCount+=1 							#track total jugglers created
-		self.joinCircuit() 							#init join circuit
+		#juggler name value
+		self.t   = data[0]					
+
+		#list of dot products		
+		self.dps = self.dotProducts(data)		
+
+		#track the preference order	
+		self.sp  = 0 						
+
+		#track total jugglers created		
+		jugglerCount+=1 					
+
+		#init join circuit		
+		self.joinCircuit() 							
 	
-	def dotProducts(self, cs):						#return the computed dot product for this juggler
+	#return the computed dot product for this juggler
+	def dotProducts(self, cs):						
 		return [ (c,( cs[1]*circuits[c].h)+(cs[2]*circuits[c].e)+(cs[3]*circuits[c].p) ) for c in cs[4:] ]
 
-	def dpsValForCirKey(self, i):					#return the value for a circuit given a key
-		for c in self.dps:							#loop jugglers preferences
-			if i == c[0]:							#if passed in circuit number equals preferred circuit in list
-				return c[1]							#return the circuit value for this preference
+	#return the value for a circuit given a key
+	def dpsValForCirKey(self, i):				
 
-	def dpsValForIndex(self, i):					#return the preference value at index
+		#loop jugglers preferences	
+		for c in self.dps:	
+
+			#if passed in circuit number equals preferred circuit in list						
+			if i == c[0]:							
+
+				#return the circuit value for this preference
+				return c[1]							
+
+	#return the preference value at index
+	def dpsValForIndex(self, i):					
 		return self.dps[i][1]		
 
-	def dpsKeyForIndex(self, i):					#return the preference key at index
+	#return the preference key at index
+	def dpsKeyForIndex(self, i):					
 		return self.dps[i][0]
 
-	def joinCircuit(self):							#enter juggler into its first preferred cicuit.
+	#enter juggler into its first preferred cicuit.
+	def joinCircuit(self):							
 		circuits[self.dpsKeyForIndex(self.sp)].hazeInductee(self)
 
-	def prettyPrint(self):							#print out self data for humans to understand
+	#print out self data for humans to understand
+	def prettyPrint(self):							
 		return "J"+str(self.t)+" C"+str(self.dps[0][0])+":"+str(self.dps[0][1])+" C"+str(self.dps[1][0])+":"+str(self.dps[1][1])+" C"+str(self.dps[1][0])+":"+str(self.dps[1][1])
 
+#read an input file
+def readFile():		
 
+	#open input file			
+	return open("data.txt")							
 
-def readFile():										#read an input file
-	return open("data.txt")							#open input file
-
+#write the output file
 def writeFile():
-	f = open('output.txt', 'w')						#write the output file
-	for c in circuits:								#print out the circuits in a pretty fashin
-		f.write("C"+str(c.t)+" ")					#write the circuit name once before the matches
-		for i in range(len(c.matches)):				#loop the circuits matches
-			f.write(c.matches[i].prettyPrint())		#conjoin our useable data back to pretty human bs
-			if i < len(c.matches)-1:				#add ,'s to the end of each line, except the last
-				f.write(", ")						#write , to end of jugglers dps
-		f.write('\n')								#write newline
-	f.close()										#close the file
+	f = open('output.txt', 'w')						
+	for c in circuits:		
+
+		#write newline						
+		f.write("C"+str(c.t)+" ")					
+		for i in range(len(c.matches)):				
+			f.write(c.matches[i].prettyPrint())		
+			if i < len(c.matches)-1:				
+				f.write(", ")						
+		f.write('\n')	
+
+	#close the file				
+	f.close()										
  
+#escape chars for line input
 def escapedChars(l):
- 	l = re.findall(r'\d+', l)  						#replace all but number chars
- 	l = [int(i) for i in l] 						#convert all chars to ints
- 	return l 										#return the [int]
+
+	#replace all but number chars
+ 	l = re.findall(r'\d+', l)  				
+
+ 	#convert all chars to ints		
+ 	l = [int(i) for i in l] 				
+
+ 	#return the [ints]		
+ 	return l 										
 
 def openCircuit():
 	f = readFile()
-	t = True	   									#track the top of file
-	for l in f: 									#read line in file
-		if t:										#check is this the top?
-			if l == '\n': 							#check for top/bottom seperator
-				t = False							#flip flag for top to False
-				continue							#skip the spac
 
-			l = escapedChars(l)						#escape the bs
-			circuit = Circuit(l)					#create a circuit
-			circuits.append(circuit)				#add the circuit to a list
+	#track the top of file
+	t = True	   									
+	for l in f: 									
+		if t:						
+			#check for top/bottom seperator				
+			if l == '\n': 							
+				t = False				
+
+				#skip the space			
+				continue							
+
+			#escape the chars
+			l = escapedChars(l)			
+
+			#create a circuit			
+			circuit = Circuit(l)		
+
+			#add the circuit to a list			
+			circuits.append(circuit)				
 		else:
-			l = escapedChars(l)						#escape the bs
-			juggler = Juggler(l)					#create a juggler that starts joining circuits
+
+			#escape the chars
+			l = escapedChars(l)						
+
+			#create a juggler that starts joining circuits
+			juggler = Juggler(l)					
 
 def main(): 	
 	global jugglerCount, circuitCount, hazeCount
 
 	print "Welcome to the JuggleFest..."
 
-	openCircuit()									#open the main circuit - to begin the competition
-	writeFile()										#write the output file
+	#open the main circuit - to begin the competition
+	openCircuit()									
 
-	for c in circuits:								#print out the circuits in a pretty fashin
-		print ""									#give the top a little breathing room from the results
+	#write the output file
+	writeFile()										
+
+	#print out the circuits in a pretty fashin
+	for c in circuits:				
+
+		#give the top a little breathing room from the results				
+		print ""									
 		for m in c.matches:
-													#conjoin our useable data back to pretty human bs
+													
+			#conjoin our useable data back to pretty human bs
 			print "C"+str(c.t), m.prettyPrint()+",",
 
-	print '\n'										#THE END
+	#THE END
+	print '\n'										
 	print "Thanks for playing Juggle Fest!\n{0}: Jugglers Attended {1} circuits.\nCombined they got their asses hazed {2} times".format(jugglerCount, circuitCount, hazeCount)
 	print '\n'
 
